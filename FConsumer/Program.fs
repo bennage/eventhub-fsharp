4e1d77e1bcd42e2b63af47e7fd9ca0f5f33e5906
Christopher Bennage <christopher@bennage.com>
2015-04-17 09:28:30 -0700

    simple event hubs in F#

---
+++ FConsumer/Program.fs
@@ -0,0 +1,52 @@
+﻿open Microsoft.ServiceBus.Messaging;
+open System.Threading.Tasks;
+open System.Text;
+open System;
+
+let sbNamespace = "your service bus namespace"
+let saName = "your service bus SAS name" 
+let saKey = "your service bus SAS key"
+
+let eventHubPath = "the name of your event hub"
+
+let storageName = "your storage account for checkpointing"
+let storageKey = "your storage account key"
+
+let eventHubCS = sprintf "Endpoint=sb://%s.servicebus.windows.net/;SharedAccessKeyName=%s;SharedAccessKey=%s" sbNamespace saName saKey
+let checkpointCS = sprintf "DefaultEndpointsProtocol=http;AccountName=%s;AccountKey=%s;" storageName storageKey
+
+let hostName = Guid.NewGuid().ToString()
+let consumgerGroup = EventHubConsumerGroup.DefaultGroupName
+
+let emptyTask() = Task.FromResult(true) :> Task
+
+type MyConsumer() =
+    interface IEventProcessor with
+        member x.OpenAsync (context) = 
+            printfn "open partition %s" context.Lease.PartitionId
+            emptyTask()
+
+        member x.CloseAsync (context, reason) = 
+            printfn "close partition %s" context.Lease.PartitionId
+            printfn "reason %A" reason
+            emptyTask()
+
+        member x.ProcessEventsAsync(context, events) = 
+            printfn "processing partition %s" context.Lease.PartitionId
+            events 
+                |> Seq.map (fun e -> e.GetBytes() |> Encoding.ASCII.GetString )
+                |> Seq.iter (printfn "%s")
+            emptyTask()
+
+[<EntryPoint>]
+let main argv = 
+    printfn "consume events"
+
+    let options = EventProcessorOptions()
+    options.ExceptionReceived.AddHandler(fun s a -> printfn "boom!" )
+
+    let host = EventProcessorHost (hostName, eventHubPath, consumgerGroup, eventHubCS, checkpointCS)
+    host.RegisterEventProcessorAsync<MyConsumer>(options) |> ignore
+    printfn "finished registering"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ FProducer/Program.fs
@@ -0,0 +1,36 @@
+﻿open Microsoft.ServiceBus.Messaging;
+open System.Threading.Tasks;
+open System.Text;
+open System;
+open System.Reactive;
+
+let sbNamespace = "your service bus namespace"
+let saName = "your service bus SAS name" 
+let saKey = "your service bus SAS key"
+
+let eventHubPath = "the name of your event hub"
+let eventHubCS = sprintf "Endpoint=sb://%s.servicebus.windows.net/;SharedAccessKeyName=%s;SharedAccessKey=%s" sbNamespace saName saKey
+
+[<EntryPoint>]
+let main argv = 
+    printfn "send events"
+
+    let client = EventHubClient.CreateFromConnectionString(eventHubCS, eventHubPath)
+
+    let send = fun x ->
+        let bytes = (sprintf "some stuff %d" x) |> Encoding.ASCII.GetBytes
+        let event = new EventData(bytes)
+        client.Send(event) |> ignore
+        printfn "sent %i" x
+
+    let timer = Linq.Observable.Generate( 
+                    0,
+                    (fun _ -> true),
+                    (fun x -> x + 1),
+                    (fun x -> x),
+                    (fun x -> TimeSpan.FromSeconds(1.0)))
+    
+    let subscription = timer.Subscribe(send)
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
